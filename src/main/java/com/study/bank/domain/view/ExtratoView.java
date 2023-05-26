8174458eb1b6fc77484f6f4b64bc354309af6f60
VtOliv <vitorturmac@gmail.com>
2023-05-26 16:48:48 -0300

    tratamento de erros

--- src/main/java/com/study/bank/domain/view/ExtratoView.java
+++ src/main/java/com/study/bank/domain/view/ExtratoView.java
@@ -2,12 +2,6 @@ package com.study.bank.domain.view;
 
 import java.time.LocalDateTime;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
